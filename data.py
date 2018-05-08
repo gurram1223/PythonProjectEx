0f076350060b52ba02deb8af2e7599efcec19ff3
gurram1223 <37588274+gurram1223@users.noreply.github.com>
2018-05-09 00:28:17 +0530

    pending

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'Article One',
+            'body':"Lorem ipsum dorem im umet, consecteutor elit, sed do.",
+            'author':'Pavan',
+            'create-date':'07-May-2018'
+        },
+        {
+            'id':2,
+            'title':'Article two',
+            'body':"Lorem ipsum dorem im umet, consecteutor elit, sed do.",
+            'author':'Mahesh',
+            'create-date':'07-May-2018'
+        },
+        {
+            'id':3,
+            'title':'Article three',
+            'body':"Lorem ipsum dorem im umet, consecteutor elit, sed do.",
+            'author':'Arun',
+            'create-date':'07-May-2018'
+        }
+    ]
+    return articles
